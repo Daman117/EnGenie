e00c44e61c8df912fb84fd7fc9474f0c00f2dccb
Daman117 <Daman.Chundi@ensarsolutions.com>
2025-11-14 14:18:01 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { Button } from '../components/ui/button';
 import { Brain, CheckCircle, XCircle,ArrowLeft } from 'lucide-react';
 import { GlassCard, GlassCardContent, GlassCardHeader, GlassCardTitle } from '../components/ui/glass-card';
-
+import { BASE_URL } from '../components/AIRecommender/api';
 const API_BASE_URL = BASE_URL;
 
 const AdminDashboard = () => {
